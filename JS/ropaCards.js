c51293f61880bb41d0c840e154a09fbe6a8b4428
DianaRiosS <diana.rios.sant@gmail.com>
2023-11-02 10:36:52 -0600

    Carrito en proceso

--- JS/ropaCards.js
+++ JS/ropaCards.js
@@ -1,3 +1,9 @@
+// Define el objeto carrito en el ámbito global
+let carrito = JSON.parse(localStorage.getItem('carrito')) ||{
+    productos: [],
+    total: 0
+};
+
 // Se crea una variable app, la cual contendrá posteriormente la informacion del archivo ropa.jsonp(Este archivo contiene los datos de los 10 objetos y se define como jsonp o Json con padding, ya que permite llamar los datos contenidos mediante una funcion "callBack")
 var app ={}
 var callBack = function(datos){
@@ -30,5 +36,28 @@ var callBack = function(datos){
         });
 
         // Agrega las tarjetas al contenedor
-    document.getElementById("articles-container").innerHTML = html;
+        document.getElementById("articles-container").innerHTML = html;
+    
+        let botones = document.querySelectorAll(".cssbuttons-io");
+        botones.forEach((boton, index) => {
+            boton.addEventListener('click', () => {
+              const producto = {
+                img: app.ropa[index].img,
+                nombre: app.ropa[index].Nombre,
+                talla: app.ropa[index].Talla,
+                precio: app.ropa[index].Precio,
+                color: app.ropa[index].Color,
+              };
+              
+              carrito.productos.push(producto);
+              carrito.total += producto.precio;
+              
+              localStorage.setItem('carrito', JSON.stringify(carrito));
+              
+              console.log(carrito);
+            });
+          });
+          
+
+
 }
